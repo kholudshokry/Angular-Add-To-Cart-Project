8e8336d555ca78b77444e67a4845cecde6217fd2
kholud shokry <eng.kholud.shokry@gmail.com>
2021-01-10 21:19:01 -0800

    checkout form validation before submitting the order

--- src/app/cartdropdown/cartdropdown.component.ts
+++ src/app/cartdropdown/cartdropdown.component.ts
@@ -1,4 +1,8 @@
+
+
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { CustomvalidationService } from '../services/customvalidation.service';
 
 @Component({
   selector: 'app-cartdropdown',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartdropdownComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private customValidator: CustomvalidationService
+  ) { }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required], this.customValidator.userNameValidator.bind(this.customValidator)],
+      password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator()])],
+      confirmPassword: ['', [Validators.required]],
+    },
+      {
+        validator: this.customValidator.MatchPassword('password', 'confirmPassword'),
+      }
+    );
   }
 
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.registerForm.value);
+    }
+  }
 }
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { CustomvalidationService } from '../services/customvalidation.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,13 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
     
-   
-   
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private customValidator: CustomvalidationService
+  ) { }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator()])],
+      
+    }
+    );
   }
-delete(){
-  localStorage.clear();
-}
+
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      
+      console.table(this.registerForm.value);
+    }
+  }
+  delete(){
+    localStorage.clear();
+  }
 }
